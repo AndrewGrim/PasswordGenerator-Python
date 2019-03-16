94cba98a3a220b76380d610d2fc5d6972bf3c0e9
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2019-03-16 20:11:38 +0000

    separated gui init into a different file
    so you dont have to scroll past the mountain of gui code to get how the program works

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # PasswordGenerator-Python
 
-A password generator written in python. There are two versions one with a gui written in tkinter and one that run purely in the terminal.
+A password generator written in python. There are two versions one with a gui written in tkinter and one that runs purely in the terminal.
--- src/PasswordGenerator.py
+++ src/PasswordGenerator.py
@@ -1,51 +1,17 @@
 import random
 from tkinter import * #pylint: disable=W0614
-from tkinter import ttk
 import pyperclip
 import os
+from gui import guiInit
+#pylint: disable=W0612
+#pylint: disable=E1103
 
 class PasswordGenerator():
 
-
     def __init__(self, root):
         # the gui setup
-
-        # top frame which includes all the options such as length and each of the list from which the password is generated
-        self.passwordOptionsFrame = Frame(root)
-        self.passwordOptionsFrame.grid(row=0, column=0)
-
-        self.passwordLengthVar = IntVar(value=16)
-        self.passwordLengthButton = ttk.Spinbox(self.passwordOptionsFrame, from_=1, to=100, increment=1, textvariable=self.passwordLengthVar)
-        self.passwordLengthButton.pack()
-
-        self.includeNumbersVar = IntVar(value=1)
-        self.includeNumbersCheck = ttk.Checkbutton(self.passwordOptionsFrame, text="Include numbers", variable=self.includeNumbersVar)
-        self.includeNumbersCheck.pack(fill=X)
-
-        self.includeLowercaseVar = IntVar(value=1)
-        self.includeLowercaseCheck = ttk.Checkbutton(self.passwordOptionsFrame, text="Include lower case letters", variable=self.includeLowercaseVar)
-        self.includeLowercaseCheck.pack(fill=X)
-
-        self.includeUppercaseVar = IntVar(value=1)
-        self.includeUppercaseCheck = ttk.Checkbutton(self.passwordOptionsFrame, text="Inlcude upper case letters", variable=self.includeUppercaseVar)
-        self.includeUppercaseCheck.pack(fill=X)
-
-        self.includeSpecialVar = IntVar(value=1)
-        self.includeSpecialCheck = ttk.Checkbutton(self.passwordOptionsFrame, text="Inlcude special characters", variable=self.includeSpecialVar)
-        self.includeSpecialCheck.pack(fill=X)
-
-
-        # bottom frame which contains the generate button and the entry box used for output
-        self.outputFrame = Frame(root)
-        self.outputFrame.grid(row=1, column=0)
-
-        self.generatepasswordButton = ttk.Button(self.outputFrame, text="Generate Password", command=self.generatePassword)
-        self.generatepasswordButton.pack(side=LEFT)
-
-        self.generatePasswordVar = StringVar(value="")
-        self.generatedPasswordEntry = ttk.Entry(self.outputFrame, textvariable=self.generatePasswordVar, width=50, justify=CENTER)
-        self.generatedPasswordEntry.pack(side=RIGHT)
-
+        guiInit(self, root)
+        
 
     def generatePassword(self):
         # generates the password from the lists below of your choosing and according to the length set
@@ -74,7 +40,7 @@ class PasswordGenerator():
         if len(lists) == 0:
             self.generatePasswordVar.set(value="check at least one box!")
         else:
-            for i in range(length): #pylint: disable=W0612
+            for i in range(length): 
                 category = random.choice(lists)
                 #print(category)
                 password += random.choice(category)
@@ -106,4 +72,3 @@ except:
     print("failed to load the windows icon")
 program = PasswordGenerator(root)
 root.mainloop()
-
