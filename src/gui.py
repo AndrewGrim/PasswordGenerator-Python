863eea2870b8b58b42b72b8f0df22fdf377be1fa
Andrew Grim <andrew.grim.persona@gmail.com>
2019-03-25 12:24:11 +0000

    added support for python prior to 3.7

--- src/gui.py
+++ src/gui.py
@@ -2,13 +2,27 @@ from tkinter import * #pylint: disable=W0614
 from tkinter import ttk
 # pylint: disable=W0612
 
+class TTKSpinbox(ttk.Entry):
+    # ttk.Spinbox was omitted from python version prior to 3.7
+    # if the machine has python version prior to 3.7 this class will make it work as intended
+
+    def __init__(self, master=None, **kw):
+
+        ttk.Entry.__init__(self, master, "ttk::spinbox", **kw)
+    def set(self, value):
+        self.tk.call(self._w, "set", value)
+
 def guiInit(self, root):
     # top frame which includes all the options such as length and each of the list from which the password is generated
     self.passwordOptionsFrame = Frame(root)
     self.passwordOptionsFrame.grid(row=0, column=0)
 
     self.passwordLengthVar = IntVar(value=16)
-    self.passwordLengthButton = ttk.Spinbox(self.passwordOptionsFrame, from_=1, to=100, increment=1, textvariable=self.passwordLengthVar)
+    try:
+        self.passwordLengthButton = ttk.Spinbox(self.passwordOptionsFrame, from_=1, to=100, increment=1, textvariable=self.passwordLengthVar)
+    except:
+        self.passwordLengthButton = TTKSpinbox(self.passwordOptionsFrame, from_=1, to=100, increment=1, textvariable=self.passwordLengthVar)
+        print("python version older than 3.7")
     self.passwordLengthButton.pack()
 
     self.includeNumbersVar = IntVar(value=1)
